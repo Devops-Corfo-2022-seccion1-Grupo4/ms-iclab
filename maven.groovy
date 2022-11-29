ad2af23f9e39b7aacc2341b2c0fc1f33f5d8231a
contadxrrrzzz <71387693+contadxrrrzzz@users.noreply.github.com>
2022-11-29 19:24:56 -0300

    Update maven.groovy

--- maven.groovy
+++ maven.groovy
@@ -1,11 +1,43 @@
-def exec(){
-  
-    echo 'Building..'
-    sh "mvn clean install"
-    echo 'SonarQube..'
-    withSonarQubeEnv('Sonar'){
-		  sh 'mvn clean package sonar:sonar'
-    }
-}
+def llamarPipeline()
+{
 
-return this;
+stage('TestApp') {
+
+ sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+}
+}
+stage('Compile') {
+      sh './mvnw clean compile -e'
+     }
+
+     stage('Test') {
+      sh './mvnw clean test -e'
+    }
+    stage('Jar') {
+      sh './mvnw clean package -e'
+    }
+  stage('Run') {
+    sh 'nohup bash mvnw spring-boot:run &'
+  sleep 20
+   }
+  
+   
+  stage ('Sonarqube Analisis') {
+    withSonarQubeEnv('Sonar'){
+		sh 'mvn clean package sonar:sonar'
+    }
+  }
+    stage('SonarQualityGate') {
+    timeout(time: 1, unit: 'HOURS') {
+                    waitForQualityGate abortPipeline: true
+    }
+    }
+    
+
+	
+
+
+
+
+return this
+
