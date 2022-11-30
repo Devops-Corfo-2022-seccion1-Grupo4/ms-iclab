79fee3a22838811598f109b95062fa8c4fa76a27
Cristobal Valencia <cristobalvalenciaq@gmail.com>
2022-11-29 21:24:06 -0300

    cambioTag.sh changes

--- trabajo/cambioTag.sh
+++ trabajo/cambioTag.sh
@@ -6,10 +6,6 @@ if [[ $# -eq 3 ]]; then
         path="$3";
 sed -i "12,14 s/$actual/$nuevo/g" $path/pom.xml
 else 
-<<<<<<< HEAD
 	echo "error";
-=======
-        echo "error";
->>>>>>> a21525d447ceef000d3b50c11d5ee7870ea4b6e3
 fi
 exit 0
